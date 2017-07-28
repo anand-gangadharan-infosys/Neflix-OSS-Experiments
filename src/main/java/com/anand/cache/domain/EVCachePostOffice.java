8a808c19587fe7253c7b49b14187e42627fb639a
Anand Gangadharan <anand_gangadharan@infosys.com>
2017-07-28 16:24:17 +0530

    EVCache functional

--- src/main/java/com/anand/cache/domain/EVCachePostOffice.java
+++ src/main/java/com/anand/cache/domain/EVCachePostOffice.java
@@ -29,40 +29,17 @@ public class EVCachePostOffice {
 		System.setProperty("CACHE-POSTCODES-NODES", deploymentDescriptor);
 
 		evCache = new EVCache.Builder().setAppName("CACHE-POSTCODES").build();
-		//evCache = new EVCache.Builder().setAppName("CACHE-POSTCODES").setCachePrefix("prefix").enableRetry()
-		//		.setDefaultTTL(300).build();
-
-		// Debug
-		setKey("695025",
-				new PostalLocation("695025", "Muttada S.O", "Thiruvananthapuram", "Thiruvananthapuram", "KERALA"),500);
-		setKey("695143",
-				new PostalLocation("695143", "Palachira B.O", "Chirayinkeezhu", "Thiruvananthapuram", "KERALA"),500);
-		setKey("695603",
-				new PostalLocation("695603", "Parakunnu B.O", "Chirayinkeezhu", "Thiruvananthapuram", "KERALA"),500);
-		PostalLocation loc = PostalLocation.getObj(evCache.get("695025"));
-		System.out.println("Anand 1-"+ loc);
 	}
 
-	public PostalLocation getPostalLocation(String pincode) throws EVCacheException, JsonParseException, JsonMappingException, IOException {
-		
+	public PostalLocation getPostalLocation(String pincode)
+			throws EVCacheException, JsonParseException, JsonMappingException, IOException {
+
 		return PostalLocation.getObj(evCache.get(pincode));
 	}
 
 	public void setKey(String key, PostalLocation value, int timeToLive) throws Exception {
 		try {
-			Future<Boolean>[] _future = evCache.set(key, value.toString(), timeToLive);
-
-			// Wait for all the Futures to complete.
-			// In "verbose" mode, show the status for each.
-			for (Future<Boolean> f : _future) {
-				System.out.println("Future "+f);
-				boolean didSucceed = f.get();
-				System.out.println("per-shard set success code for key " + key + " is " + didSucceed);
-			}
-			System.out.println("finished setting key " + key);
-			PostalLocation loc =  PostalLocation.getObj(evCache.get("695025"));
-			System.out.println("Anand 2-"+ loc);
-
+			evCache.set(key, value.toString(), timeToLive);
 		} catch (EVCacheException e) {
 			e.printStackTrace();
 		}
