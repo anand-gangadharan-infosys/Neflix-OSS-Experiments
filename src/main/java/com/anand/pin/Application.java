1ee39cd917b137bf491b1d3ee4126cb1be68b434
Anand Gangadharan <anand_gangadharan@infosys.com>
2017-08-01 16:41:21 +0530

    Registered Both service pin and cache to eureka

--- src/main/java/com/anand/pin/Application.java
+++ src/main/java/com/anand/pin/Application.java
@@ -4,9 +4,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+
+
 
 @SpringBootApplication(scanBasePackages = { "com.anand" })
+@EnableEurekaClient
 public class Application {
 	private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
 
