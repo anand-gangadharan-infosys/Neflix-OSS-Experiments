ba604931324097524ca058d996070d48ea25cdf4
Anand Gangadharan <anand_gangadharan@infosys.com>
2017-08-04 12:34:31 +0530

    Added a DB Dumb

--- src/main/java/com/anand/pin/client/PinClient.java
+++ src/main/java/com/anand/pin/client/PinClient.java
@@ -2,11 +2,12 @@ package com.anand.pin.client;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import com.anand.common.model.PostalLocation;
 
-@Component
+@Service
 public class PinClient {
 
 	@Autowired
