c77a734c51a711ab77184b96f32da0c1f40e78be
Anand Gangadharan <anand_gangadharan@infosys.com>
2017-08-04 19:01:58 +0530

    Sanitize the deploy script

--- megadeploy.sh
+++ megadeploy.sh
@@ -1,28 +1,55 @@
 #!/bin/bash
 
-function build(){
-  gradle -p infrastructure/eureka clean build
-  gradle -p services/com/anand/pin clean build
-  gradle -p services/com/anand/cache clean build
+function buildApp(){
+  project=$1
+  gradle -p $project clean build
+}
+function startApp(){
+  project=$1
+  app=$2
+  version=$3
+  buildApp  $project
+  java -jar $project/build/libs/$app-$version.jar >> logs/$app.log 2>>logs/$app.log &
+}
+function killprocesslisteningon(){
+  port=$1
+  jar=$2
+
+  echo Find pids listening on $port
+
+  # For debug list the processes listening
+  lsof -i tcp:$port|awk 'NR >= 2{ print "Killing process "$1" with PID "$2 }'
+  #Kill it
+  kill -9 `lsof -i tcp:$port| awk 'NR >= 2 {print $2}' | sort -n|uniq` 2>>/dev/null
+
+  echo 'Checking for java process with jar '$jar
+
+  # Just check if any process with the jar in any inconsistent state
+  ps -ax|grep java|grep $jar|awk '{Killing java process found at PID $1}'
+  #Kill it
+  kill -9 `ps -ax|grep java|grep $jar|awk '{print $1}' | sort -n|uniq` 2>>/dev/null
+
 }
 
 function start(){
-  build
-  java -jar infrastructure/eureka/build/libs/eureka-0.1.0.jar>>logs/eureka.log 2>>logs/eureka.log &
-  sleep 11
-  java -jar services/com/anand/pin/build/libs/pin-0.1.0.jar>>logs/pin.log 2>>logs/pin.log&
   source infrastructure/ev-deploy-descriptor.sh
-  java -jar services/com/anand/cache/build/libs/cache-0.1.0.jar>>logs/cache.log 2>>logs/cache.log&
+  startApp 'infrastructure/eureka' 'eureka' '0.1.0'
+  sleep 11
+  startApp 'services/com/anand/pin' 'pin' '0.1.0'
+  startApp 'services/com/anand/cache' 'cache' '0.1.0'
   echo 'Service Started'sort
 }
 
 function stop(){
-  kill -9 `lsof -i tcp:8080|awk {'print $2'} | sort -n|uniq` 2>>/dev/null
-  kill -9 `lsof -i tcp:8081|awk {'print $2'} | sort -n|uniq` 2>>/dev/null
-  kill -9 `lsof -i tcp:8761|awk {'print $2'} | sort -n|uniq` 2>>/dev/null
+  killprocesslisteningon 8080 'pin-0.1.0.jar'
+  killprocesslisteningon 8081 'cache-0.1.0.jar'
+  killprocesslisteningon 8761 'eureka-0.1.0.jar'
   echo 'Service Stoped'
 }
-
+#------------------------------------------------------------------------------#
+##Start of Script
+#------------------------------------------------------------------------------#
+#==============================================================================#
 
 case $1 in
 start)
