ee73430a4f080e8dd7682173cf3298edb4525bff
Jince Tom Varghese <jince.varghese@ip-192-168-1-4.eu-west-1.compute.internal>
2017-09-12 23:51:48 +0530

    fixing the bug in the implementaion of feign client

--- src/main/java/com/anand/cache/Application.java
+++ src/main/java/com/anand/cache/Application.java
@@ -2,35 +2,22 @@ package com.anand.cache;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
-
-import com.anand.pin.client.PinClient;
-import com.anand.pin.client.PinClientFallback;
 
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = { "com.anand" })
 @EnableDiscoveryClient
 @EnableCircuitBreaker
-@EnableFeignClients
+@EnableFeignClients(basePackages = {"com.anand"})
 public class Application {
 	private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
 
 	public static void main(String[] args) throws InterruptedException {
 		SpringApplication.run(Application.class, args);
-	}
-	
-	@Bean
-	public PinClient pinClient(){
-		return new PinClientFallback();
 	}
 }
