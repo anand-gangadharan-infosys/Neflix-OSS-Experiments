ea03b52c039bce06453390339e087bb1942d2485
Jince Tom Varghese <jince.varghese@ip-10-155-213-67.eu-west-1.compute.internal>
2017-09-13 17:11:09 +0530

    added corsfilter

--- infrastructure/gateway/src/main/java/gateway/Application.java
+++ infrastructure/gateway/src/main/java/gateway/Application.java
@@ -13,6 +13,9 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -112,5 +115,23 @@ public class Application {
               };
           }
       };
+  }
+  
+  @Bean
+  public CorsFilter corsFilter() {
+      final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+      final CorsConfiguration config = new CorsConfiguration();
+      config.setAllowCredentials(true);
+      config.addAllowedOrigin("*");
+      config.addAllowedHeader("*");
+      config.addAllowedMethod("OPTIONS");
+      config.addAllowedMethod("HEAD");
+      config.addAllowedMethod("GET");
+      config.addAllowedMethod("PUT");
+      config.addAllowedMethod("POST");
+      config.addAllowedMethod("DELETE");
+      config.addAllowedMethod("PATCH");
+      source.registerCorsConfiguration("/**", config);
+      return new CorsFilter(source);
   }
 }
