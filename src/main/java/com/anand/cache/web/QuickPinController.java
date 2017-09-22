8fa925e02ca89887eedfcc6e8887843364a9058f
Anand Gangadharan <anand_gangadharan@infosys.com>
2017-09-22 19:34:13 +0530

    Added script for rabbit MQ start

--- src/main/java/com/anand/cache/web/QuickPinController.java
+++ src/main/java/com/anand/cache/web/QuickPinController.java
@@ -2,12 +2,14 @@ package com.anand.cache.web;
 
 import java.io.IOException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.metrics.CounterService;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
 
 import com.anand.cache.domain.CacheMissException;
 import com.anand.cache.domain.EVCachePostOffice;
@@ -25,11 +27,18 @@ public class QuickPinController {
 
 	@Autowired
 	private PinClient pinClient;
+	
+	@Autowired
+	private CounterService counterService;	
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(QuickPinController.class);
 
 	@RequestMapping(method = RequestMethod.GET, value = "/postal/search/{pin}")
 	public PostalLocation getPinLocation(@PathVariable(value = "pin") String pinCode)
 			throws EVCacheException, JsonParseException, JsonMappingException, IOException {
-
+		
+		counterService.increment("postal.caching.total");
+		
 		try {
 			PostalLocation location = cacheDataStore.getPostalLocation(pinCode);
 			if (location != null) {
@@ -45,6 +54,7 @@ public class QuickPinController {
 	
 	private PostalLocation fetchFromDB(String pinCode) {
 		System.out.println("Cache miss");
+		counterService.increment("postal.caching.cachemiss");
 		PostalLocation location = pinClient.getPinCode(pinCode);
 		if (location != null && location.getPostOfficeName()!= null) {
 			saveToCache(pinCode, location);
